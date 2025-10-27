8f6194a68b9d25d4c2b4f0bc543b109c933fed28
sangii99 <Sang.Yoon@ugent.be>
2025-10-27 20:07:59 +0100

    ex1
    ex1

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:dmlaneo4627&@db.eggbqllnmdtwvhcaoiva.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
